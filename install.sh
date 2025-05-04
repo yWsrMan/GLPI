5f190a8fbfa3b2e32b75ee1ae1a7be20c444c546
yWsrMan <151638956+yWsrMan@users.noreply.github.com>
2025-05-04 16:35:26 +0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+
+MYSQL_ROOT_USER="root"
+MYSQL_ROOT_PASSWORD="123"
+
+GLPI_DB="glpi"
+GLPI_USER="glpi"
+GLPI_PASSWORD="yourstrongpassword"
+
+#
+apt update && apt upgrade -y
+apt install -y apache2 php php-{apcu,cli,common,curl,gd,imap,ldap,mysql,xmlrpc,xml,mbstring,bcmath,intl,zip,redis,bz2} libapache2-mod-php php-soap php-cas
+apt install -y mariadb-server
+
+#
+mysql_secure_installation <<EOF
+
+n
+Y
+$MYSQL_ROOT_PASSWORD
+$MYSQL_ROOT_PASSWORD
+Y
+Y
+Y
+Y
+EOF
+
+#
+mysql_tzinfo_to_sql /usr/share/zoneinfo | mysql -u$MYSQL_ROOT_USER -p$MYSQL_ROOT_PASSWORD mysql
+
+#
+mysql -u$MYSQL_ROOT_USER -p$MYSQL_ROOT_PASSWORD <<EOF
+CREATE DATABASE $GLPI_DB;
+CREATE USER '$GLPI_USER'@'localhost' IDENTIFIED BY '$GLPI_PASSWORD';
+GRANT ALL PRIVILEGES ON $GLPI_DB.* TO '$GLPI_USER'@'localhost';
+GRANT SELECT ON mysql.time_zone_name TO '$GLPI_USER'@'localhost';
+FLUSH PRIVILEGES;
+EOF
+
+#
+cd /var/www/html
+wget https://github.com/glpi-project/glpi/releases/download/10.0.18/glpi-10.0.18.tgz
+tar -xvzf glpi-10.0.18.tgz
+
+#
+cat << 'EOF' > /var/www/html/glpi/inc/downstream.php
+<?php
+define('GLPI_CONFIG_DIR', '/etc/glpi/');
+if (file_exists(GLPI_CONFIG_DIR . '/local_define.php')) {
+require_once GLPI_CONFIG_DIR . '/local_define.php';
+}
+EOF
+
+#
+mv /var/www/html/glpi/config /etc/glpi
+mv /var/www/html/glpi/files /var/lib/glpi
+mv /var/lib/glpi/_log /var/log/glpi
+
+#
+cat << 'EOF' > /etc/glpi/local_define.php
+<?php
+define('GLPI_VAR_DIR', '/var/lib/glpi');
+define('GLPI_DOC_DIR', GLPI_VAR_DIR);
+define('GLPI_CRON_DIR', GLPI_VAR_DIR . '/_cron');
+define('GLPI_DUMP_DIR', GLPI_VAR_DIR . '/_dumps');
+define('GLPI_GRAPH_DIR', GLPI_VAR_DIR . '/_graphs');
+define('GLPI_LOCK_DIR', GLPI_VAR_DIR . '/_lock');
+define('GLPI_PICTURE_DIR', GLPI_VAR_DIR . '/_pictures');
+define('GLPI_PLUGIN_DOC_DIR', GLPI_VAR_DIR . '/_plugins');
+define('GLPI_RSS_DIR', GLPI_VAR_DIR . '/_rss');
+define('GLPI_SESSION_DIR', GLPI_VAR_DIR . '/_sessions');
+define('GLPI_TMP_DIR', GLPI_VAR_DIR . '/_tmp');
+define('GLPI_UPLOAD_DIR', GLPI_VAR_DIR . '/_uploads');
+define('GLPI_CACHE_DIR', GLPI_VAR_DIR . '/_cache');
+define('GLPI_LOG_DIR', '/var/log/glpi');
+EOF
+
+#
+chown root:root /var/www/html/glpi/ -R
+chown www-data:www-data /etc/glpi -R
+chown www-data:www-data /var/lib/glpi -R
+chown www-data:www-data /var/log/glpi -R
+chown www-data:www-data /var/www/html/glpi/marketplace -Rf
+find /var/www/html/glpi/ -type f -exec chmod 0644 {} \;
+find /var/www/html/glpi/ -type d -exec chmod 0755 {} \;
+find /etc/glpi -type f -exec chmod 0644 {} \;
+find /etc/glpi -type d -exec chmod 0755 {} \;
+find /var/lib/glpi -type f -exec chmod 0644 {} \;
+find /var/lib/glpi -type d -exec chmod 0755 {} \;
+find /var/log/glpi -type f -exec chmod 0644 {} \;
+find /var/log/glpi -type d -exec chmod 0755 {} \;
